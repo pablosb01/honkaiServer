4718f4aac41756dedb647effa46d06e08411ee83
pablosb01 <pablosb01@icloud.com>
2024-06-17 16:52:43 +0200

    server fetch

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+async function honkaiInfo(user) {
+    const res = await fetch(`htpps://api.mihomo.me/sr_info_parsed/${user}`);
+    const data = await res.json();
+    return data;
+}
+
+app.get("honkai/:user", async (req, res) => {
+    const { user } = req.params;
+    try {
+        const info = await honkaiInfo(id)
+        res.send(info);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.listen(3000, () => console.log('running'))
